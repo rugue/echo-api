cbf967103dbbb9be67d20cafc5b88c5808afe8cb
Osarugue Enehizena <nehirugue@gmail.com>
2024-12-25 23:54:15 +0100

    add users module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/echo')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/echo'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
