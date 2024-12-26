e440998fa82254eefdf9e1ed060d1443b5cedc14
Osarugue Enehizena <nehirugue@gmail.com>
2024-12-26 01:25:58 +0100

    add class-validator and class-transformer for user DTO validation; update CreateUserDto with validation decorators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsOptional()
+  @IsString()
   role?: string;
+
+  @IsOptional()
+  @IsString()
   name?: string;
 }
