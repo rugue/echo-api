a8430e2969b565ab08e622b1983d3c0e91de7b5d
Osarugue Enehizena <nehirugue@gmail.com>
2024-12-31 01:20:39 +0100

    add posts module with controller, service, and DTOs for post management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SettingsModule } from './settings/settings.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/echo'), UsersModule, SettingsModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/echo'), UsersModule, SettingsModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
