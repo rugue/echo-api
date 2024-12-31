8be3f9294323dda690a7f6353391c38e1cd984ea
Osarugue Enehizena <nehirugue@gmail.com>
2024-12-31 18:41:25 +0100

    refactor posts module: remove unused Post entity, implement Post schema, and update PostsService for MongoDB integration

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Post } from './schema/post.schema';
 
 @Injectable()
 export class PostsService {
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
