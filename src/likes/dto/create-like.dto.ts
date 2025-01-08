6d8af5b412a78a583d73796e7f294aafd5ecdf7b
Osarugue Enehizena <nehirugue@gmail.com>
2025-01-08 12:12:30 +0100

    feat(likes): enhance CreateLikeDto and Like entity with validation and Swagger documentation

--- src/likes/dto/create-like.dto.ts
+++ src/likes/dto/create-like.dto.ts
@@ -1,1 +1,14 @@
-export class CreateLikeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateLikeDto {
+  @ApiProperty({ description: 'The ID of the user' })
+  @IsString()
+  @IsNotEmpty()
+  user: string;
+
+  @ApiProperty({ description: 'The ID of the song' })
+  @IsString()
+  @IsNotEmpty()
+  song: string;
+}
--- src/likes/entities/like.entity.ts
+++ src/likes/entities/like.entity.ts
@@ -1,1 +1,24 @@
-export class Like {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/users/schemas/user.entity';
+import { Song } from 'src/songs/entities/song.entity';
+
+export type LikeDocument = Like & Document;
+
+@Schema({ timestamps: true })
+export class Like {
+  @ApiProperty({ description: 'The user who liked the song' })
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'User', required: true })
+  user: User;
+
+  @ApiProperty({ description: 'The liked song' })
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'Song', required: true })
+  song: Song;
+
+  @ApiProperty({ description: 'The date the song was liked' })
+  @Prop({ type: Date, default: Date.now })
+  likedAt: Date;
+}
+
+export const LikeSchema = SchemaFactory.createForClass(Like);
