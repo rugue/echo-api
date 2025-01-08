6dec174e4e9d43da233a1611b03029e50abe6000
Osarugue Enehizena <nehirugue@gmail.com>
2025-01-08 11:12:28 +0100

    feat(playlists): add Mongoose integration and export PlaylistsService

--- src/playlists/playlists.module.ts
+++ src/playlists/playlists.module.ts
@@ -3,7 +3,14 @@ import { PlaylistsService } from './playlists.service';
 import { PlaylistsController } from './playlists.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Playlist.name, schema: PlaylistSchema },
+    ]),
+    UsersModule,
+  ],
   controllers: [PlaylistsController],
   providers: [PlaylistsService],
+  exports: [PlaylistsService],
 })
 export class PlaylistsModule {}
