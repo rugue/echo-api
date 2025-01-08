51d7406074e3ee55894bf94ff83ad2e3d9fbd0ce
Osarugue Enehizena <nehirugue@gmail.com>
2025-01-08 11:10:16 +0100

    feat(playlists): enhance CreatePlaylistDto and UpdatePlaylistDto with validation and Swagger documentation

--- src/playlists/dto/create-playlist.dto.ts
+++ src/playlists/dto/create-playlist.dto.ts
@@ -1,1 +1,32 @@
-export class CreatePlaylistDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
+
+export class CreatePlaylistDto {
+  @ApiProperty({ description: 'The name of the playlist' })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'The description of the playlist',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty({
+    description: 'The cover image URL of the playlist',
+    required: false,
+  })
+  @IsUrl()
+  @IsOptional()
+  coverImageUrl: string;
+
+  @ApiProperty({
+    description: 'The ID of the user associated with the playlist',
+  })
+  @IsString()
+  @IsNotEmpty()
+  user: string;
+}
--- src/playlists/dto/update-playlist.dto.ts
+++ src/playlists/dto/update-playlist.dto.ts
@@ -1,4 +1,26 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreatePlaylistDto } from './create-playlist.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdatePlaylistDto extends PartialType(CreatePlaylistDto) {}
+export class UpdatePlaylistDto extends PartialType(CreatePlaylistDto) {
+  @ApiProperty({ description: 'The name of the playlist', required: false })
+  name?: string;
+
+  @ApiProperty({
+    description: 'The description of the playlist',
+    required: false,
+  })
+  description?: string;
+
+  @ApiProperty({
+    description: 'The cover image URL of the playlist',
+    required: false,
+  })
+  coverImageUrl?: string;
+
+  @ApiProperty({
+    description: 'The ID of the user associated with the playlist',
+    required: false,
+  })
+  user?: string;
+}
